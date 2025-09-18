e77968e20f902b47644d82e56c49c48f582823b0
laraagustinadevoto22-code <laraagustinadevoto22@gmail.com>
2025-09-18 14:46:11 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php require "partials/header.php" ?>
+
+<div class="container mt-5">
+    <div class="col-6 mx-auto">
+        <h1>Bienvenidos A LA CLASE DE ESI: </h1>
+        <form action="def-palabra.php"></form>
+        <label for="palabra">Elige una palabra</label>
+        <select name="palabra" id="palabra">
+            <option value="Autocuidado">Autocuidado</option>
+            <option value="Respecto">Respeto</option>
+            <option value="Amistad">Amistad</option>
+
+        </select>
+
+        </form>
+    </div>
+</div>
+
+<?php require "partials/footer.php" ?>
