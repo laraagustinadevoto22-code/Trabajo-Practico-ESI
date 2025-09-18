e679c178ccf044e7d0121019a64e94b73d89161f
laraagustinadevoto22-code <laraagustinadevoto22@gmail.com>
2025-09-18 16:50:06 -0300

    first index

--- index.php
+++ index.php
@@ -3,30 +3,17 @@
 <div class="container mt-5">
     <div class="col-6 mx-auto">
         <h1>Bienvenidos A LA CLASE DE ESI: </h1>
-<<<<<<< HEAD
-        <form action="def-palabra.php" method="post">
-        <label for="palabra">Elige una palabra</label>
-        <select name="palabra" id="palabra">
-=======
         <form action="def-palabra.php" method="post"></form>
         <label for="palabra">Elige una palabra</label>
         <select name="palabra" id="palabra"> 
->>>>>>> b86e5574cb97fc326a11880f93007e2234e07c5a
             <option value="autocuidado">Autocuidado</option>
             <option value="respeto">Respeto</option>
             <option value="amistad">Amistad</option>
 
         </select>
-<<<<<<< HEAD
-
-         <input type="submit" value="ver mensaje">
-
-=======
 <button type="button" class="btn btn-primary">Ver</button>
->>>>>>> b86e5574cb97fc326a11880f93007e2234e07c5a
         </form>
     </div>
 </div>
-
 
 <?php require "partials/footer.php" ?>
